057ce5d2a8a057b35c63c6d3fdf67b67843a8d34
SteelBoshido99 <55700855+SteelBoshido99@users.noreply.github.com>
2021-01-14 04:39:36 +0000

    Final commit V2
    Just a small adjustment showing the Stock Data manager inheriting the Stock Class

--- src/KioskClasses/stockDataManager.java
+++ src/KioskClasses/stockDataManager.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class stockDataManager {
+public class stockDataManager extends Admin {
 
     public String stockPath = "resources\\Stock.txt";
     public String separator = "\\|";
